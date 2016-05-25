141af819e4cf2cd924e8c3e0ee322ea68cde55de
Masanori Usami <nichiyoubinoshisya@gmail.com>
2016-05-26 02:05:54 +0900

    ECHOプログラム（クライアント）追加

--- project/pc_line_tracer/src/pc_line_tracer/PCLineTracer.java
+++ project/pc_line_tracer/src/pc_line_tracer/PCLineTracer.java
@@ -17,7 +17,33 @@ public class PCLineTracer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Socket socket_  = null;
+		final int PORT_ = 12345;
+		
+		try {
+			socket_ = new Socket(args[0], PORT_);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket_.getInputStream()));
+			PrintWriter out  = new PrintWriter(socket_.getOutputStream());
+			BufferedReader keyin = new BufferedReader(new InputStreamReader(System.in));
+			
+			String input;
+			while((input = keyin.readLine()).length() > 0) {
+				out.println(input);
+				String line = in.readLine();
+				if (line != null) {
+					System.out.println(line);
+				}
+				else {
+					break;
+				}
+			}
+		}
+		catch(UnknownHostException e1) {
 
+		}
+		catch(IOException e2) {
+			
+		}
 	}
 
 }
