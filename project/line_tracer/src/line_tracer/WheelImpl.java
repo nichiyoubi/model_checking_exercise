75ea89dbb5cfcd33b21734fcf85938a636e86ab9
Masanori Usami <nichiyoubinoshisya@gmail.com>
2016-06-02 00:49:31 +0900

    WheelImplの動作をsynchronizedに変更

--- project/line_tracer/src/line_tracer/WheelImpl.java
+++ project/line_tracer/src/line_tracer/WheelImpl.java
@@ -31,4 +31,31 @@ public class WheelImpl extends EV3LargeRegulatedMotor implements Wheel {
 	public int getMinimumSpeed() {
 		return 0;
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see lejos.hardware.motor.BaseRegulatedMotor#forward()
+	 */
+	@Override
+	public synchronized void forward() {
+		super.forward();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see lejos.hardware.motor.BaseRegulatedMotor#backward()
+	 */
+	@Override
+	public synchronized void backward() {
+		super.backward();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see lejos.hardware.motor.BaseRegulatedMotor#stop()
+	 */
+	@Override
+	public synchronized void stop() {
+		super.stop();
+	}
 }
