0208a3065ad8f716227acfe5469bff3dde688103
Masanori Usami <nichiyoubinoshisya@gmail.com>
2016-07-31 10:50:04 +0900

    コメントアウトしていた処理を復帰させた

--- project/line_tracer/src/line_tracer/LineTracer.java
+++ project/line_tracer/src/line_tracer/LineTracer.java
@@ -27,18 +27,14 @@ public class LineTracer {
 		TouchSensorImpl touch = new TouchSensorImpl(SensorPort.S1);
 
 		/// ネットワーク・リモート制御スレッドの起動
-/*
 		RemoteController remoteController = new RemoteController(direction);
 		Thread threadController = new Thread(remoteController);
 		threadController.start();
-*/
 		
 		///　ネットワーク・データ送信スレッドの起動
-/*
 		RemoteDataProvider dataProvider = new RemoteDataProvider(light, rightWheel, leftWheel);
 		Thread threadProvider = new Thread(dataProvider);
 		threadProvider.start();
-*/
 		
 		RemoteObstacleDetector detector = new RemoteObstacleDetector(sonic, touch);
 		Thread threadDetector = new Thread(detector);
@@ -51,7 +47,7 @@ public class LineTracer {
 	public static void trace(LightSensor light, Controller controller, DirectionController direction) {
 		Key enter   = ((EV3)BrickFinder.getLocal()).getKey("Enter");
 		
-		light.setThreashold(0.20F);
+		light.setThreashold(0.15F);
 		
 		LCD.drawString("Push Start.", 0, 2);
 		while(enter.isUp()) {
@@ -61,7 +57,7 @@ public class LineTracer {
 		
 		for(int i = 0; i < 1200; i++) {
 			Delay.msDelay(100);
-//			controller.execute();
+			controller.execute();
 			Float val = light.getValue();
 			LCD.drawString("light=" + String.valueOf(val), 0, 3);
 			if (enter.isDown()) {
